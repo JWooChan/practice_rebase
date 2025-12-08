a40b096ae227b2dcdd14ec5d2c8a8e48581cbae4
JWooChan <jwc6584@naver.com>
2025-12-08 15:38:02 +0900

    Implement ByteTrack class initial
    add reset

---
+++ tracker/bytetrack.py
@@ -0,0 +1,19 @@
+"""ByteTrack Implementation"""
+
+class ByteTrack:
+    """Multi-object tracker using ByteTrack algorithm"""
+    
+    def __init__(self, track_thresh=0.5, track_buffer=30, match_thresh=0.8):
+        self.track_thresh = track_thresh
+        self.track_buffer = track_buffer
+        self.match_thresh = match_thresh
+        self.tracked_objects = []
+
+    def update(self, detections):
+        """Update tracks with new detections"""
+        # TODO: implement later
+        pass
+
+    def reset(self):
+        """Reset all tracks"""
+        self.tracked_objects = []
